34a7108425d8b178d4d5ea4fbae6e52163c887b6
Brasero <brandonricci4@gmail.com>
2025-11-27 20:25:42 +0100

    passge de browser router à hash router

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateDraw from "./pages/CreateDraw";
 import ShareDraw from "./pages/ShareDraw";
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={"/santa-s-secret-draw"}>
+      <HashRouter basename={"/santa-s-secret-draw"}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateDraw />} />
@@ -27,7 +27,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
