d4ec185dd8059f6acb9520b70461eda6e0d53719
Brasero <brandonricci4@gmail.com>
2025-11-27 19:48:28 +0100

    ajout du basename

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={"/santa-s-secret-draw"}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateDraw />} />
